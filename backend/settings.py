cf1bf3f2c225358a0e65ac37d937a682ed0223fb
jaishankar g <jaishankarg24@gmail.com>
2021-03-16 12:31:45 +0530

    settings djangoheroku

--- backend/settings.py
+++ backend/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -182,3 +184,5 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 #if os.getcwd() == '/app':
    # DEBUG = False
+
+django_heroku.settings(locals())
